f4a2d6aa06b29d2528ef28cd5ab9b4a72b4112e7
ganes <ganes@JanaGokul>
2025-07-22 20:45:21 +0530

    Encapulation & scanner

---
+++ training/src/Day3/Encapsulation.java
@@ -0,0 +1,15 @@
+package Day3;
+
+public class Encapsulation {
+public static void main(String[] args) {
+		
+		//object creation 
+		Oopsconcept obj = new Oopsconcept();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+}
