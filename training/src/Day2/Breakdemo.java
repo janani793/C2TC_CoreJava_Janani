fcaf4a9149d4f81d3c520a4f83f136cb396006a4
ganes <ganes@JanaGokul>
2025-07-22 09:01:28 +0530

    Training

---
+++ training/src/Day2/Breakdemo.java
@@ -0,0 +1,15 @@
+package Day2;
+
+public class Breakdemo {
+	public static void main(String[] args) {
+
+		for (int i = 5; i < 10; i++) {
+			if (i == 5)
+				break;
+			System.out.println(i);
+		}
+
+	}
+}
+
+
