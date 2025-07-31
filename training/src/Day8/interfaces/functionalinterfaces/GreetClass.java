b50248732a95b08ee00caedc853042747f31c8f4
ganes <ganes@JanaGokul>
2025-07-31 10:09:35 +0530

    CoreJava

--- training/src/Day8/interfaces/functionalinterfaces/GreetClass.java
+++ training/src/Day8/interfaces/functionalinterfaces/GreetClass.java
@@ -1,11 +1,4 @@
 package Day8.interfaces.functionalinterfaces;
 
 
-	public class GreetClass implements GreetInterface {
-
-		
-		public String Greet() {		
-			return "Welcome to the world of Java";
-		}
-
-}
+	
