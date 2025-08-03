06fb9e1f266d3c38d00ad85b117cbb20bea92f98
ganes <ganes@JanaGokul>
2025-08-03 19:58:10 +0530

    CoreJava

---
+++ training/src/Day12/InvalidMarkException.java
@@ -0,0 +1,13 @@
+package Day12;
+
+	public class InvalidMarkException extends Exception{
+
+		public InvalidMarkException() {
+			super();
+		}
+
+		public InvalidMarkException(String message) {
+			super(message);
+		}
+
+}
